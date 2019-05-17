4f03e3351536ccde332f8670ee689cc2bc8c3962
Marcelo Del Fiore <marcelodelfiore@gmail.com>
2019-05-17 15:48:39 -0300

    Update frame format and saving data on Slicing Dice data warehouse

--- basic_client.py
+++ basic_client.py
@@ -1,45 +1,60 @@
 import paho.mqtt.client as paho
-import time
-import datetime
+import requests
+
 """
- Basic client connecting to Cloud MQTT
+ Basic client connecting to Cloud MQTT and savind data on Slicing Dice
  
 """
-MQTT_FRAME_SIZE = 6
-
 data_list = []
-data_pointer = 0
+
+topic1 = "eqpto1/#"
 
 broker_address = 'm15.cloudmqtt.com'
 broker_port = 12460
 broker_user_name = 'fjobiivi'
 broker_password = 'ik8o68Dsnlgk'
 
+MASTER_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfX3NhbHQiOiIxNTU4MTE2NDI4MTk4IiwicGVybWlzc2lvbl9sZXZlbCI6MywicHJvamVjdF9pZCI6NDExOTEsImNsaWVudF9pZCI6NDMxfQ.lvhx6NkFXnLFP1hv6JMoNZvDGFThw5crVzQ1RdAIigM"
+sd_insert_endpoint_url = "https://api.slicingdice.com/v1/insert"
+sd_insert_header = {'Authorization': MASTER_API_KEY, 'content-type': 'application/json'}
 
-def update_database(topic, data):
 
-  print(f'Tópico: {topic}, Data: {data}')
+def update_database(id, data):
+
+  insert_data = {
+    id: {
+      "va": data[0],
+      "vb": data[1],
+      "vc": data[2],
+      "pa": data[3],
+      "pb": data[4],
+      "pc": data[5]
+    }
+  }
+
+  resp = requests.post(url=sd_insert_endpoint_url, json=insert_data, headers=sd_insert_header)
+
 
 # callback functions
 def on_connect(client, userdata, flags, rc):
   print("Connected with code " + str(rc))
-  client.subscribe("eqpto1/#")
+  client.subscribe(topic1)
 
 
 def on_message(client, userdata, msg):
 
-  topic = (str(msg._topic))[-3:-1]
+  topic = (str(msg._topic)).strip("'b")
   payload = str(msg.payload)
 
-  if topic in ['va', 'vb', 'vc']:
-    data = payload.strip("'b")
-  elif topic in ['pa', 'pb', 'pc']:
-    data = payload.strip("'b")
-  elif topic in ['ts']:
-    data = payload.strip("'b")
-    #data = time.ctime(int(payload.strip("'b")))
+  _, va,_, vb, _, vc, _, pa, _, pb, _, pc, _, ts = payload.split(";")
+  ts = ts.strip("'b")
 
-  update_database(topic, data)
+  unique_ID = topic + ":" + ts
+
+  data_list[:] = (int(va), int(vb), int(vc), int(pa), int(pb), int(pc))
+
+  update_database(unique_ID, data_list)
+
 
 if __name__ == '__main__':
 
@@ -53,4 +68,3 @@ if __name__ == '__main__':
   client.connect(broker_address, broker_port, 60)
 
   client.loop_forever()
-
